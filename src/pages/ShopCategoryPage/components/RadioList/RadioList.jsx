cdb94fe8593d53c2dd7a2fd774b4df6ec290448b
Le Hong Quan <lehongquan15a@gmail.com>
2023-08-02 07:06:28 +0700

    [ReactJS] - Lesson15

--- src/pages/ShopCategoryPage/components/RadioList/RadioList.jsx
+++ src/pages/ShopCategoryPage/components/RadioList/RadioList.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import { Input } from "reactstrap";
 import "./RadioList.css";
 
-export const RadioList = ({ data }) => {
+export const RadioList = ({ data, selected, handleSelected }) => {
   return (
     <ul className="radio-list">
       {data.map((item, index) => (
         <li key={index} className="radio-list__item">
-          <Input type="radio" id={item.id} name={item.type} className="me-3" />
+          <Input
+            type="radio"
+            id={item.id}
+            name={item.type}
+            value={item.id}
+            className="me-3"
+            selected={selected === item.id}
+            onChange={(e) => {
+              handleSelected(parseInt(e.target.value));
+            }}
+          />
           <label htmlFor={item.id}>
             {item.name}
             <span> ({item.quantity})</span>
